7325cc71531d6689bb7dec087a02d81d77631b0c
Adam Siekierski <adam.siekierski@appunite.com>
2021-12-24 10:50:49 +0100

    :construction: Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+#  Kolorowo Upstrzone Testy Algorytmów Sortowania
+
+## Wykorzystane technologie
+
+- C++
+- SFML
+- Projekt napisany jest w Xcode (w planach dodanie cmake)
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,6 @@
-
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-// Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
 int main(int, char const**)
